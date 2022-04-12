6797f7836b60a68bd170b6ac0c81bea5e53e5f50
hbitancur <hbitancur@bienesdelestado.gob.ar>
2022-04-11 23:28:26 -0300

    Ejercicio 5

--- app/src/main/java/com/desafio1/alkeparking/Main.kt
+++ app/src/main/java/com/desafio1/alkeparking/Main.kt
@@ -1,5 +1,22 @@
 package com.desafio1.alkeparking
 
+import java.util.*
+
 fun main() {
+    val car = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001")
+    val moto = Vehicle("B222BBB", VehicleType.MOTO, Calendar.getInstance())
+    val minibus = Vehicle("CC333CC", VehicleType.MINIBUS, Calendar.getInstance())
+    val bus = Vehicle("DD444DD", VehicleType.BUS, Calendar.getInstance(), "DISCOUNT_CARD_002")
+    val car2 = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_003")
+
+    val parking = Parking(mutableSetOf(car, moto, minibus, bus))
+    val isCar2Inserted = parking.vehicles.add(car2)
+
+    println(parking.vehicles.contains(car))
+    println(parking.vehicles.contains(moto))
+    println(parking.vehicles.contains(minibus))
+    println(parking.vehicles.contains(bus))
+
+    println(isCar2Inserted)
 
 }
--- app/src/main/java/com/desafio1/alkeparking/Parking.kt
+++ app/src/main/java/com/desafio1/alkeparking/Parking.kt
@@ -1,5 +1,10 @@
 package com.desafio1.alkeparking
 
 data class Parking(val vehicles: MutableSet<Vehicle>) {
+    var availableSpot: Int = 20
+
+    fun addVehicle(vehicle: Vehicle): Boolean {
+        return (availableSpot - vehicles.size) > 0
+    }
 
 }
