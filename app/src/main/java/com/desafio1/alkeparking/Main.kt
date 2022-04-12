dbc0b2091c72e85aa85e7e84c03dced8c61088a2
hbitancur <hbitancur@bienesdelestado.gob.ar>
2022-04-11 23:43:31 -0300

    Ejercicio 6

--- app/src/main/java/com/desafio1/alkeparking/Main.kt
+++ app/src/main/java/com/desafio1/alkeparking/Main.kt
@@ -4,19 +4,38 @@ import java.util.*
 
 fun main() {
     val car = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001")
-    val moto = Vehicle("B222BBB", VehicleType.MOTO, Calendar.getInstance())
+    val moto = Vehicle("BB222BB", VehicleType.MOTO, Calendar.getInstance())
     val minibus = Vehicle("CC333CC", VehicleType.MINIBUS, Calendar.getInstance())
     val bus = Vehicle("DD444DD", VehicleType.BUS, Calendar.getInstance(), "DISCOUNT_CARD_002")
-    val car2 = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_003")
 
     val parking = Parking(mutableSetOf(car, moto, minibus, bus))
-    val isCar2Inserted = parking.vehicles.add(car2)
 
-    println(parking.vehicles.contains(car))
-    println(parking.vehicles.contains(moto))
-    println(parking.vehicles.contains(minibus))
-    println(parking.vehicles.contains(bus))
+    val listOfVehicles = listOf(
+        car, moto, minibus, bus,
+        Vehicle("EE555EE", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("FF666FF", VehicleType.MOTO, Calendar.getInstance()),
+        Vehicle("GG777GG", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_003"),
+        Vehicle("HH888HH", VehicleType.BUS, Calendar.getInstance()),
+        Vehicle("II999II", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("JJ000JJ", VehicleType.MOTO, Calendar.getInstance()),
+        Vehicle("KK111KK", VehicleType.MINIBUS, Calendar.getInstance()),
+        Vehicle("LL222LL", VehicleType.BUS, Calendar.getInstance()),
+        Vehicle("MM333MM", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("NN444NN", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("OO555OO", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_004"),
+        Vehicle("PP666PP", VehicleType.MOTO, Calendar.getInstance()),
+        Vehicle("QQ777QQ", VehicleType.BUS, Calendar.getInstance()),
+        Vehicle("RR888RR", VehicleType.MINIBUS, Calendar.getInstance()),
+        Vehicle("SS999SS", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("TT000TT", VehicleType.BUS, Calendar.getInstance()),
+        Vehicle("UU111UU", VehicleType.BUS, Calendar.getInstance(), "DISCOUNT_CARD_005"),
+        Vehicle("VV222VV", VehicleType.BUS, Calendar.getInstance()),
+        Vehicle("WW333WW", VehicleType.CAR, Calendar.getInstance()),
+        Vehicle("XX444XX", VehicleType.MOTO, Calendar.getInstance()),
+    )
 
-    println(isCar2Inserted)
+    listOfVehicles.map {
+        println(parking.addVehicle(it))
+    }
 
 }
--- app/src/main/java/com/desafio1/alkeparking/Parking.kt
+++ app/src/main/java/com/desafio1/alkeparking/Parking.kt
@@ -1,10 +1,16 @@
 package com.desafio1.alkeparking
 
 data class Parking(val vehicles: MutableSet<Vehicle>) {
-    var availableSpot: Int = 20
+    private val availableSpot: Int = 20
 
-    fun addVehicle(vehicle: Vehicle): Boolean {
-        return (availableSpot - vehicles.size) > 0
+    fun addVehicle(vehicle: Vehicle): String {
+        return when(availableSpot - vehicles.size) {
+            in 1..availableSpot -> {
+                vehicles.add(vehicle)
+                "Welcome to AlkeParking!"
+            }
+            else -> "Sorry, the check-in failed"
+        }
     }
 
 }
