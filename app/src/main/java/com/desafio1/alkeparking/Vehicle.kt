fe9d8836cb1c91256b26b0ccf2e52a7be2317cac
hbitancur <hbitancur@bienesdelestado.gob.ar>
2022-04-11 22:57:35 -0300

    Ejercicio 3 y 4

--- app/src/main/java/com/desafio1/alkeparking/Vehicle.kt
+++ app/src/main/java/com/desafio1/alkeparking/Vehicle.kt
@@ -1,7 +1,17 @@
 package com.desafio1.alkeparking
 
+import java.util.*
+
+const val MINUTES_IN_MILISECONDS = 60000
+
 data class Vehicle(val plate: String,
-                   val type: VehicleType) {
+                   val type: VehicleType,
+                   val checkInTime: Calendar,
+                   var discountCard: String? = null) {
+
+    val parkedTime: Long
+        get() = (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+
     override fun equals(other: Any?): Boolean {
         if (other is Vehicle) {
             return this.plate == other.plate
